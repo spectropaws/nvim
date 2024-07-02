0dfdb2c003b8e3051a72089a2881cf347579f920
spectropaws <spectropaws777@gmail.com>
2024-07-02 23:25:27 +0530

    add snippet engine

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,17 +1,20 @@
 return {
     "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
-        local none_ls = require("null-ls")
-        none_ls.setup({
+        local null_ls = require("null-ls")
+        null_ls.setup({
             sources = {
-                none_ls.builtins.formatting.stylua,
-                none_ls.builtins.formatting.prettier,
-                none_ls.builtins.formatting.black,
-                none_ls.builtins.formatting.isort,
-                none_ls.builtins.diagnostics.eslint_d,
-            }
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.formatting.prettier,
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                require("none-ls.diagnostics.eslint_d"),
+            },
         })
 
-        vim.keymap.set('n', '<leader>cf', vim.lsp.buf.format, {})
-    end
+        vim.keymap.set("n", "<leader>cf", vim.lsp.buf.format, {})
+    end,
 }
